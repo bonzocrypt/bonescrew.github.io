ec0d64bd125089be56396ed6b4c4d510a1050821
bonzocrypt <156837947+bonzocrypt@users.noreply.github.com>
2025-04-16 12:22:29 -0400

    Add files via upload

---
+++ images/RemoteStats.js
@@ -0,0 +1,38 @@
+  function SetRemoteStats(targetPage,tableName)
+  {
+    if(!getCookie("sessionID"))
+    {
+    	var dateObj = new Date()
+    	var sid = dateObj.getTime();
+  	setCookie("sessionID", sid);
+    }	
+  
+    if(!getCookie(targetPage))
+    {
+    	document.write('<img border="0" height="1" width="1" src="http://h000102296078.ne.client2.attbi.com/remotestats/basic.asp?TargetPage=' + targetPage + '&TableName=' + tableName + '&SessionID=' + getCookie("sessionID") + '&HTTP_REFERER=' + escape(document.referrer) + '&AppName=' + escape(navigator.appName) + '&ScreenSize=' + escape(screen.width + 'x' + screen.height) + '&ScreenColorDepth=' + escape(screen.colorDepth) + '"');
+  	setCookie(targetPage, "1");
+    }
+  }
+
+  function getCookie(name) {
+      var prefix = name + "="
+      var cookieStartIndex = document.cookie.indexOf(prefix)
+      if (cookieStartIndex == -1)
+         return null
+      var cookieEndIndex = document.cookie.indexOf(";", cookieStartIndex +
+         prefix.length)
+      if (cookieEndIndex == -1)
+         cookieEndIndex = document.cookie.length
+      return unescape(document.cookie.substring(cookieStartIndex +
+         prefix.length,
+   cookieEndIndex))
+   }
+   
+   function setCookie(name, value) {
+      var curCookie = name + "=" + escape(value)
+      if ((name + "=" + escape(value)).length <= 4000)
+         document.cookie = curCookie
+      else
+         if (confirm("Cookie exceeds 4KB and will be cut!"))
+            document.cookie = curCookie
+   }
